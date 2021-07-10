10f06ea05fc5da911791e81a0ece475fca423c40
이중원 <>
2021-07-10 19:34:24 +0900

    과제 수정2(signup)

--- src/main/java/com/sparta/assignment/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/assignment/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sparta.assignment.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,6 +17,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
--- src/main/java/com/sparta/assignment/service/UserService.java
+++ src/main/java/com/sparta/assignment/service/UserService.java
@@ -25,11 +25,12 @@ public class UserService {
         // 회원 ID 중복 확인
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
-            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
+            return;
         }
         String password = passwordEncoder.encode(requestDto.getPassword());
 
         User user = new User(username, password);
         userRepository.save(user);
+
     }
 }
